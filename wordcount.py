3b620ea787fd8a4c16f2684c38fb2116216fd1f5
mark sass <marksass@marks-mbp.mynetworksettings.com>
2024-06-10 12:26:14 -0700

    First commit

---
+++ wordcount.py
@@ -0,0 +1,21 @@
+"""Count words in file."""
+
+def count_words(file_name):
+    word_counts = {}
+    file = open(file_name)
+
+    for line in file: #gives us entire line from file
+        line = line.rstrip()
+
+        words = line.split() # returns list of individual words
+        print(words)
+        for word in words:
+            word_counts[word] = word_counts.get(word, 0) + 1
+
+    # close(file)
+    return word_counts
+
+word_count_list = count_words('test.txt').items()
+# print(word_count_list) #-> keys only
+# for line, word in word_count_list:
+#     print(line, word)
